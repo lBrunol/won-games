0c2add1d0efd9b229b809fc876f3c816e3ba976b
Bruno Araújo <bruno.ar.avl@gmail.com>
2022-03-13 18:05:05 -0300

    feat: updating storybook config

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,10 +1,11 @@
 import Main from '.'
 import { withKnobs, text } from '@storybook/addon-knobs'
+import { Meta, Story } from '@storybook/react/types-6-0'
 
 export default {
   title: 'Main',
   component: Main,
   decorators: [withKnobs]
-}
+} as Meta
 
-export const Basic = () => <Main title={text('Title', 'React Avançadão')} body={text('Body', 'É isso ai')} />
+export const Basic: Story = () => <Main title={text('Title', 'React Avançadão')} body={text('Body', 'É isso ai')} />
